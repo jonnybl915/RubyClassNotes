af20470d51e9e11e6b985bf229d93feed321d5ad
Jon Black <jblack@JolleCocoon.local>
2017-01-11 11:15:25 -0500

    more hash exercises

--- hashes.rb
+++ hashes.rb
@@ -33,6 +33,7 @@ def letter_frequencies(str)
       counts[letter] = 0
     end
     counts[letter] += 1
+
   end
   return counts
 end
@@ -47,3 +48,52 @@ puts "#{letter_count(str, 'a')} a's"
 freq = letter_frequencies(str)
 puts "There are #{freq['a']} many a's"
 
+counts = letter_frequencies(File.read './warmups.rb')
+counts.delete(" ") 
+puts counts
+
+# how many different letters are there?
+
+puts "Different Letters"
+
+key_count = 0
+counts.each do |_|
+  key_count += 1
+end
+
+puts key_count
+puts counts.length
+
+puts "Total Length"
+
+total = 0
+counts.each do |key, value| # when iterating(eaching) through a hash you need two arguments!!
+  total += value;
+
+end
+puts total
+puts counts.values.reduce(:+) #double check Reduce method!
+
+puts "How many letters show up an even number of times?"
+
+total = 0
+counts.each do |key, value|
+  if value.even?
+    total += 1
+  end
+end
+puts total
+puts counts.values.select{|v| v.even? }.length
+
+evens = counts.select {|k, v| v.even?}
+puts evens
+puts evens.length
+
+puts "Most times a letter shows up"
+puts counts.values.max
+puts counts.sort_by { |k,v| -v }.to_s
+puts counts.max_by { |k,v| v}
+
+puts counts.max_by do |k, v|
+
+end
