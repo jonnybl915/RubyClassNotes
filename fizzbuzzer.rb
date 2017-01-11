e4e862220ac49958c45976c070a2f103a0ccbaa6
Jon Black <jblack@JolleCocoon.local>
2017-01-11 17:22:18 -0500

    moving rather slowly

--- fizzbuzzer.rb
+++ fizzbuzzer.rb
@@ -7,10 +7,17 @@ class Fizzbuzzer
   def run(number)
 
     i = 1
-    
+
+#use logic to print i when the rule matches
+#if any of the rules work or multiple rules
+#max (1 .. max).each do ...
+#max.times do 
+#upto(max) do
+
     while i <= number
 
       @replacements.each do |k, v|
+
 
         (i % k == 0) ? (puts v) : (puts i)
 
