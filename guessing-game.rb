2e432c703f5ce5bf7f3e8a8e6bac41974643796c
Jon Black <jblack@JolleCocoon.local>
2017-01-11 14:06:55 -0500

    added methods to GuessingGame class

--- guessing-game.rb
+++ guessing-game.rb
@@ -1,7 +1,8 @@
 class GuessingGame
-  def initialize
-    @answer = 70 # rand 1 .. 100
-    @tries_left = 5
+  def initialize(max)
+    @max = max
+    @answer = rand 1 .. max
+    @tries_left = 5 + max / 100 - 1
 
 
   end
@@ -10,24 +11,26 @@ class GuessingGame
   end
 
   def got_answer?
-    @n == @answer70
+    @guess == @answer
   end
 
-  def ask_for_num?
-    print "Please guess a number between 1 and 100: "
-    @guess = gets.chomp.to_i
+  def ask_for_num
+    print "Please guess a number between 1 and #{@max}: "
+    gets.chomp.to_i
 
   end
 
-  def check_num
+  def check_num(guess)
     @tries_left -= 1
 
     puts "Tries Left: #{@tries_left}"
 
-    if @guess == @answer
+    if guess == @answer
       puts "By George! You've Done It!!"
-    elsif @guess > @answer
+
+    elsif guess > @answer
       puts "That number is too high"
+
     else
       puts "That number is too low"
 
@@ -35,14 +38,28 @@ class GuessingGame
     end
 
   end
+
+ #attr_reader :answer
+
+  def correct_answer
+    @answer
+  end
+
 end
 
-g = GuessingGame.new
+g = GuessingGame.new 300
 
 until g.out_of_turns? || g.got_answer?
 
-  g.ask_for_num
-  g.check_num
+  guess = g.ask_for_num
+  g.check_num(guess)
+
+end
+
+if g.got_answer?
+  puts "You Have Won"
+
+else  "You Lost, the correct answer was in fact #{g.correct_answer}"
 
 end
 
