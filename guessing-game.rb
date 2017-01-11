ecc52583cbe48cae80239e48617bc77ce4007bb0
Jon Black <jblack@JolleCocoon.local>
2017-01-11 15:16:46 -0500

    added SmartAI and BadAI logic

--- guessing-game.rb
+++ guessing-game.rb
@@ -6,37 +6,29 @@ class GuessingGame
 
 
   end
+  def max
+    @max
+
+  end
   def out_of_turns?()
       @tries_left == 0
   end
 
   def got_answer?
-    @guess == @answer
-  end
-
-  def ask_for_num
-    print "Please guess a number between 1 and #{@max}: "
-    gets.chomp.to_i
-
+      @won
   end
 
   def check_num(guess)
     @tries_left -= 1
-
-    puts "Tries Left: #{@tries_left}"
-
     if guess == @answer
-      puts "By George! You've Done It!!"
-
-    elsif guess > @answer
-      puts "That number is too high"
-
+      @won = true
+    elsif guess < @answer
+      puts 'Too low!'
+      :low
     else
-      puts "That number is too low"
-
-
+      puts 'Too high!'
+      :high
     end
-
   end
 
  #attr_reader :answer
@@ -47,19 +39,70 @@ class GuessingGame
 
 end
 
-g = GuessingGame.new 300
+class Player
+  def initialize(_)
+  def ask_for_num game
+
+    print "Please guess a number between 1 and #{game.max}: "
+    gets.chomp.to_i
+
+  end
+end
+end
+class SmartAI
+  def initialize game
+    @min = 1
+    @max = game.max
+  end
+  def ask_for_num game
+    @last_guess = (@max + @min) / 2
+    puts "Guessing '#{@last_guess}'"
+
+  end
+
+  def record_result result
+    if result == :low
+      @min = @last_guess
+    elsif result == :high
+      @max = @last_guess
+    end
+  end
+end
+
+class BadAI
+  def initialize(_)
+
+  end
+  def ask_for_num game
+    guess = rand 1 .. game.max
+    puts "Guessing '#{guess}'"
+  end
+
+end
+
+g = GuessingGame.new 100
+#p = BadAI.new g
+l = SmartAI.new g
 
 until g.out_of_turns? || g.got_answer?
 
-  guess = g.ask_for_num
-  g.check_num(guess)
+  guess = l.ask_for_num g
+  result = g.check_num(guess.to_i)
+  l.record_result result
 
 end
 
 if g.got_answer?
+
   puts "You Have Won"
 
 else  "You Lost, the correct answer was in fact #{g.correct_answer}"
 
 end
+
+# what percentage of the time does the smart AI win the game
+# set up the game to play ~10,000 times and see how many result in winning
+
+
+
 
