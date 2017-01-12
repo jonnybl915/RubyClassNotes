a8a714ed34371a75cca26aacbbc4caf353d90977
Jon Black <jblack@JolleCocoon.local>
2017-01-12 08:58:30 -0500

    still working on the replace method in Fizzbuzzer class

--- fizzbuzzer.rb
+++ fizzbuzzer.rb
@@ -11,13 +11,12 @@ class Fizzbuzzer
 #use logic to print i when the rule matches
 #if any of the rules work or multiple rules
 #max (1 .. max).each do ...
-#max.times do 
+#max.times do
 #upto(max) do
 
     while i <= number
 
       @replacements.each do |k, v|
-
 
         (i % k == 0) ? (puts v) : (puts i)
 
