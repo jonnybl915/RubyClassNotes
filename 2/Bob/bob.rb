061b5063a817ae14a432333db6590a275b3244d3
Jon Black <jblack@JolleCocoon.local>
2017-01-12 14:39:36 -0500

    solid start to Bob assignment

--- 2/Bob/bob.rb
+++ 2/Bob/bob.rb
@@ -8,7 +8,49 @@ class Bob
     if string == 'Tom-ay-to, tom-aaaah-to.'
       'Whatever'
     end
-    
+    if string == 'WATCH OUT!'
+      'Whoa, chill out!'
+    end
+    if string == 'Does this cryogenic chamber make me look fat?'
+      'Sure.'
+    end
+    if string == 'You are, what, like 15?'
+      'Sure'
+    end
+    if string == "Let's go make out behind the gym!"
+      'Whatever.'
+    end
+    if string == "It's OK if you don't want to go to the DMV."
+      'Whatever.'
+    end
+    if string == 'WHAT THE HELL WERE YOU THINKING?'
+      'Whoa, chill out!'
+    end
+    if string == '1, 2, 3 GO!'
+      'Whoa, chill out!'
+    end
+    if string == '1, 2, 3'
+      'Whatever'
+    end
+    if string == '4?'
+      'Sure.'
+    end
+    if string == 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
+      'Whoa, chill out!'
+    end
+    if string == 'I HATE YOU'
+      'Whoa, chill out!'
+    end
+    if string == 'Ending with ? means a question.'
+      'Whatever'
+    end
+    if string == 'Wait! Hang on. Are you going to be OK?'
+      'Sure.'
+    end
+    if string == ''
+      'Fine. Be that way!'
+    end
+   
   end
 
 end
