14248057336ee58ca4c655483bed2971322d64de
Jon Black <jblack@JolleCocoon.local>
2017-01-17 09:34:43 -0500

    added daily warmup

---
+++ warmups/kata_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+
+class Musician
+  attr_reader :name, :instrument
+
+  def initialize name, instrument
+    @name, @instrument = name, instrument
+  end
+end
+
+class Band
+  # Write your code here!
+  attr_reader :name, :members
+
+  def initialize(name)
+    @name = name
+    @members = []
+  end
+  def add_member(member)
+    @members.push(member)
+  end
+end
+
+describe Band do
+  it 'has a name' do
+    b = Band.new 'The Beatles'
+    expect(b.name).to eq 'The Beatles'
+  end
+
+  it 'can add members' do
+    b = Band.new 'The Beatles'
+
+    expect(b.members.count).to eq 0
+
+    ringo = Musician.new('Ringo', 'Drums')
+    b.add_member ringo
+
+    expect(b.members.count).to eq 1
+  end
+end
